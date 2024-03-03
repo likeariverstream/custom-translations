a1c4034dfa2566e2c399b4fb0c51fae01b560688
Sergey Kalachev <rollstream@yandex.ru>
2024-03-03 21:48:39 +0700

    sha added

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import config from 'config'
+import { authRouter } from './components/auth/router.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(authRouter)
 
 const port = config.get('SERVER.PORT')
 app.listen(port, () => console.log(`Server started on ${port}`))
